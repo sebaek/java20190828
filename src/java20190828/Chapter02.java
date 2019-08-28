3b190bcebf9d0564d6ece01fb537bdaf86e77f92
choongang602 <602@choongang>
2019-08-28 11:45:34 +0900

    variable

--- src/java20190828/Chapter02.java
+++ src/java20190828/Chapter02.java
@@ -20,7 +20,7 @@ public class Chapter02 {
 		//    숫자형 6개
 		//      정수형 4개 byte(1), short(2), int(4), long(8)
 		//      실수형 2개 float, double
-		//    문자형 1개 char
+		//    문자형 1개 char(2)
 		//    논리형 1개 boolean
 		//
 		// 2. 참조형데이터 타입
@@ -53,11 +53,70 @@ public class Chapter02 {
 		System.out.println(longVar);
 		
 		
+		// float: 4byte로 표현
+		float floatVar;
+		floatVar = 3.14F;
+		System.out.println(floatVar);
+		
+		// double: 8byte로 표현
+		double doubleVar;
+		doubleVar = 3.14;
+		System.out.println(doubleVar);
+		
+		// char: 2byte로 표현
+		char charVar;
+		char charVar2;
+		char charVar3;
+		char charVar4;
+		char charVar5;
+		charVar = '자';
+		charVar2 = '바';
+		charVar3 = 'J';
+		charVar4 = 65;
+		charVar5 = '\uac00';
+		System.out.print(charVar);
+		System.out.println(charVar2);
+		System.out.println(charVar3);
+		System.out.println(charVar4);
+		System.out.println(charVar5);
+		
+		// boolean
+		boolean booleanVar;
+		booleanVar = true;
+		booleanVar = false;
+		System.out.println(booleanVar);
 		
 		
 		
+		// * 타입변환
+		int intVar1 = 129;
+		byte byteVar1;
+		byteVar1 = (byte) intVar1; //강제 형변환 (type conversion)
+		System.out.println(byteVar1);
+		
+		int intVar2 = 125;
+		byte byteVar2;
+		byteVar2 = (byte) intVar2; //강제 형변환 (type conversion)
+		System.out.println(byteVar2);
+		
+		byte byteVar3 = 125;
+		int intVar3 = byteVar3;
+		System.out.println(intVar3);
 		
 		
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
