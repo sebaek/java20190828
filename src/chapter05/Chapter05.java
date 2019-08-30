fe7c995582c2de9cb69760e21dc1b6328c63f1fe
choongang602 <602@choongang>
2019-08-30 14:30:05 +0900

    2dimension array

--- src/chapter05/Chapter05.java
+++ src/chapter05/Chapter05.java
@@ -44,8 +44,35 @@ public class Chapter05 {
 		System.out.println(strArray1[1] == strArray2[0]);
 		System.out.println(strArray1[1].equals(strArray2[0]));
 		
-
+		//다차원 배열 
+		int[] array5 = {33, 44, 55};
+		int[][] array6;
+		array6 = new int[][] { 
+					new int[] {44, 55},
+					new int[] {99, 88, 77},
+					new int[] {23, 45, 67, 99}
+		};
 		
+		int[] array7 = array6[0];
+		for (int l = 0; l < 2; l++ ) {
+			System.out.println(array7[l]);
+		}
+		int[] array8 = array6[1];
+		for (int l = 0; l < 3; l++ ) {
+			System.out.println(array8[l]);
+		}
+		int[] array9 = array6[2];
+		for (int l = 0; l < 4; l++ ) {
+			System.out.println(array9[l]);
+		}
+		
+		System.out.println("=============================");
+		for (int l = 0; l < array6.length; l++) {
+			for (int m = 0; m < array6[l].length; m++) {
+				System.out.println(array6[l][m]);
+			}
+		}
+		array6[2][3] = 103;
 		
 		
 		
