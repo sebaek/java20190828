5d7e5d550b13a9c2796f27c2202ff9981e96650c
choongang602 <602@choongang>
2019-08-30 15:27:28 +0900

    chapter05

---
+++ src/chapter05/ArrayCopyExample.java
@@ -0,0 +1,14 @@
+package chapter05;
+
+public class ArrayCopyExample {
+	public static void main(String[] args) {
+		String[] oldStrArray = {"java", "array", "copy"};
+		String[] newStrArray = new String[5];
+		
+		System.arraycopy(oldStrArray, 0, newStrArray, 2, oldStrArray.length);;
+		
+		for (int i = 0; i < newStrArray.length; i++) {
+			System.out.print(newStrArray[i] + ", ");
+		}
+	}
+}
