0399752cef47fa1e18620ce0e990a5aa8c9293d3
choongang602 <602@choongang>
2019-09-05 12:11:46 +0900

    chapter 8

--- src/chapter08/RemoteControlExample.java
+++ src/chapter08/RemoteControlExample.java
@@ -4,7 +4,32 @@ public class RemoteControlExample {
 	public static void main(String[] args) {
 		RemoteControl rc;
 		rc = new Television();
-		rc = new Audio();
+//		rc = new Audio();
+		
+		RemoteControl.changeBattery();
+		System.out.println(RemoteControl.MAX_VOLUME);
+		rc.setMute(false);
+		
+		rc.turnOn();
+		rc.turnOff();
+		rc.setVolume(10);
+		
+		Searchable st = new SmartTelevision();
+		SmartTelevision st2 = (SmartTelevision) st;
+		RemoteControl rc2 = (RemoteControl) st;
+		
+//		Searchable st3 = (Searchable) rc; 
+		
+		
+		
+//		st.turnOn();
+//		st.turnOff();
+//		st.setVolume(5);
+//		st.search("");
+		
+		
+		
+		
 		
 		rc = new RemoteControl() {
 
