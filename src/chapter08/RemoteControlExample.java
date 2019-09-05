13159e701fe344c8f1a962e8d20c15dec26c465a
choongang602 <602@choongang>
2019-09-05 11:10:46 +0900

    chapter 8

--- src/chapter08/RemoteControlExample.java
+++ src/chapter08/RemoteControlExample.java
@@ -5,5 +5,30 @@ public class RemoteControlExample {
 		RemoteControl rc;
 		rc = new Television();
 		rc = new Audio();
+		
+		rc = new RemoteControl() {
+
+			@Override
+			public void turnOff() {
+			}
+
+			@Override
+			public void turnOn() {
+			}
+
+			@Override
+			public void setVolume(int volume) {
+			}
+			
+		};
+		
+		rc.setVolume(10);
 	}
 }
+
+
+
+
+
+
+
---
+++ src/chapter08/Searchable.java
@@ -0,0 +1,5 @@
+package chapter08;
+
+public interface Searchable {
+	void search(String url);
+}
