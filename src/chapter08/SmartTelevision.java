0d1aa423b8af2226f82d9aa22893758e0eb1d247
choongang602 <602@choongang>
2019-09-05 11:23:15 +0900

    chapter 8

--- src/chapter08/SmartTelevision.java
+++ src/chapter08/SmartTelevision.java
@@ -2,4 +2,28 @@ package chapter08;
 
 public class SmartTelevision implements RemoteControl, Searchable {
 
+	@Override
+	public void search(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
