e595b6bedaed8564719eae798a4689a720c41a78
choongang602 <602@choongang>
2019-09-06 14:13:36 +0900

    findstring

--- src/quiz/FindString.java
+++ src/quiz/FindString.java
@@ -7,11 +7,29 @@ import java.util.Scanner;
 public class FindString {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner scanner = new Scanner(new File("jean.txt"));
-		
+		String word = "a";
+		int count = 0;
+
 		while (scanner.hasNextLine()) {
-			System.out.println(scanner.nextLine());
+//			System.out.println(scanner.nextLine());
+			String line = "";
+			int index = -1;
+
+			line = scanner.nextLine().toLowerCase();
+
+			while (true) {
+				index = line.indexOf(word);
+				if (index >= 0) {
+					count++;
+				} else {
+					break;
+				}
+				line = line.substring(index + word.length());
+			}
 		}
-		
+
+		System.out.println(count + "개 있습니다.");
+
 		scanner.close();
 	}
 }
