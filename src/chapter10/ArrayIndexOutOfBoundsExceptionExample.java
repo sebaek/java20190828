a6c6ef013d7ed9cdb90b210303a87ce76145aaa6
choongang602 <602@choongang>
2019-09-09 09:29:36 +0900

    chapter10

---
+++ src/chapter10/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+	public static void main(String[] args) {
+		if (args.length == 2) {
+			String data1 = args[0];
+			String data2 = args[1];
+
+			System.out.println("args[0]:" + data1);
+			System.out.println("args[1]:" + data2);
+		} else {
+			System.out.println("[실행 방법]");
+			System.out.print("java ArrayIndexOutOfBoundsExceptionExample ");
+			System.out.println("값1 값2");
+		}
+	}
+}
---
+++ src/chapter10/NullPoineterExceptionExample.java
@@ -0,0 +1,8 @@
+package chapter10;
+
+public class NullPoineterExceptionExample {
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+	}
+}
