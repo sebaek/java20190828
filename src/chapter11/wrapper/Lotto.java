0117ca91bcc16ed64f431e7d629d178a3f0cb56e
choongang602 <602@choongang>
2019-09-10 16:09:48 +0900

    lotto

--- src/chapter11/wrapper/Lotto.java
+++ src/chapter11/wrapper/Lotto.java
@@ -5,32 +5,30 @@ import java.util.Random;
 
 public class Lotto {
 	public static void main(String[] args) {
-		int[] myLotto = new int[6];
-		myLotto[0] = 3;
-		myLotto[1] = 6;
-		myLotto[2] = 14;
-		myLotto[3] = 8;
-		myLotto[4] = 21;
-		myLotto[5] = 33;
-
-		long totalGame = 0;
 		Random random = new Random();
 		HashSet<Integer> set = new HashSet<>();
+		int[] myLotto = {11, 13, 1, 21, 33, 44};
+		long totalGame = 0;
 
-		while (!isWon(myLotto, set)) {
+		while (!isWon(set, myLotto)) {
 			set.clear();
 			while (set.size() < 6) {
-				set.add(random.nextInt(45) + 1); 
+//			System.out.println(random.nextInt(45) + 1);
+				set.add(random.nextInt(45) + 1);
 			}
 			totalGame++;
 		}
 		
 		System.out.println(totalGame + "주 후에 당첨");
 		System.out.println((totalGame * 7 / 365) + "년 후에 당첨");
-	}
 
-	private static boolean isWon(int[] myLotto, HashSet<Integer> set) {
-		for (int num : myLotto) {
+//		for (int num : set) {
+//			System.out.println(num);
+//		}
+	}
+	
+	private static boolean isWon(HashSet<Integer> set, int[] my) {
+		for (int num : my) {
 			if (!set.contains(num)) {
 				return false;
 			}
@@ -38,6 +36,12 @@ public class Lotto {
 		return true;
 	}
 }
+
+
+
+
+
+
 
 
 
