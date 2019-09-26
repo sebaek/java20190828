b5b621cad613e63676621d0661db5e0887a913cd
choongang602 <choongang602@exmap.com>
2019-09-26 11:17:11 +0900

    outputstream write method

--- src/chapter18/IOStream.java
+++ src/chapter18/IOStream.java
@@ -1,15 +1,68 @@
 package chapter18;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IOStream {
 	public static void main(String[] args) throws Exception {
 //		read1();
 //		read2();
-		read3();
+//		read3();
+		
+//		write1();
+//		write2();
+		write3();
+		
 	}
 	
+	
+	
+	
+	
+	public static void write3() throws Exception {
+		OutputStream os = new FileOutputStream("output.txt");
+		byte[] b = "한글".getBytes();
+		os.write(b, 2, 2);
+		
+		
+		os.flush();
+		os.close();
+	}
+
+
+
+
+
+	public static void write2() throws Exception {
+		OutputStream os = new FileOutputStream("output.txt");
+//		byte[] b = new byte[10];
+		byte[] b = "korea".getBytes();
+		os.write(b);
+		
+		os.flush();
+		os.close();
+		
+	}
+
+
+
+
+
+	public static void write1() throws Exception {
+		OutputStream os = new FileOutputStream("output.txt");
+		os.write(112);
+		os.write('A');
+		
+		
+		os.flush();
+		os.close();
+	}
+
+
+
 	public static void read3() throws Exception {
 		InputStream fis = new FileInputStream("src/chapter18/IOStream.java");
 		byte[] bytes = new byte[100];
@@ -31,6 +84,7 @@ public class IOStream {
 		byte[] bytes = new byte[100];
 		int read = fis.read(bytes);
 		System.out.println(read);
+		System.out.println(fis.read(bytes));
 		System.out.println(fis.read(bytes));
 		System.out.println(fis.read(bytes));
 		System.out.println(fis.read(bytes));
