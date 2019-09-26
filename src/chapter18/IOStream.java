0f632612205f3ad9ce7b72576a7bb0c709dfe2ac
choongang602 <choongang602@exmap.com>
2019-09-26 10:16:34 +0900

    inputstream read method

--- src/chapter18/IOStream.java
+++ src/chapter18/IOStream.java
@@ -5,9 +5,61 @@ import java.io.InputStream;
 
 public class IOStream {
 	public static void main(String[] args) throws Exception {
+//		read1();
+//		read2();
+		read3();
+	}
+	
+	public static void read3() throws Exception {
+		InputStream fis = new FileInputStream("src/chapter18/IOStream.java");
+		byte[] bytes = new byte[100];
+		fis.read(bytes, 2, 3);
+		System.out.println(bytes[0]);
+		System.out.println(bytes[1]);
+		System.out.println(bytes[2]);
+		System.out.println(bytes[3]);
+		System.out.println(bytes[4]);
+		System.out.println(bytes[5]);
+		System.out.println(bytes[6]);
+		
+		fis.close();
+	}
+	
+	public static void read2() throws Exception {
 		InputStream fis = new FileInputStream("src/chapter18/IOStream.java");
 		
+		byte[] bytes = new byte[100];
+		int read = fis.read(bytes);
+		System.out.println(read);
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
+		System.out.println(fis.read(bytes));
 		
+		
+		fis.close();
+	}
+
+	public static void read1() throws Exception {
+		InputStream fis = new FileInputStream("src/chapter18/IOStream.java");
+
+		int r = 0;
+
+		while ((r = fis.read()) != -1) {
+			System.out.print((char) r);
+		}
+
 		fis.close();
 	}
 }
