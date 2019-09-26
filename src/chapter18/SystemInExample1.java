c295351b9b64643d3735b2ae42f7e99b662d8bee
choongang602 <choongang602@exmap.com>
2019-09-26 14:29:40 +0900

    console

--- src/chapter18/SystemInExample1.java
+++ src/chapter18/SystemInExample1.java
@@ -6,9 +6,11 @@ public class SystemInExample1 {
 	public static void main(String[] args) throws Exception {
 		
 		InputStream is = System.in;
-		
+		System.out.print("입력:");
 		int inputChar =  is.read();
 		
 		System.out.println(inputChar);
+		System.out.println(is.read());
+		System.out.println(is.read());
 	}
 }
