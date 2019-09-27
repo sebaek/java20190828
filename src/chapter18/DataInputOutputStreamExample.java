f0d5696989e4e9ddc6a07a80f1b79288230ae882
choongang602 <choongang602@exmap.com>
2019-09-27 10:33:56 +0900

    data Stream

---
+++ src/chapter18/DataInputOutputStreamExample.java
@@ -0,0 +1,45 @@
+package chapter18;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class DataInputOutputStreamExample {
+	public static void main(String[] args) throws Exception {
+		FileOutputStream fos = new FileOutputStream("prim.dat");
+		DataOutputStream dos = new DataOutputStream(fos);
+		
+		dos.writeDouble(3.5);
+		dos.writeDouble(.5);
+		dos.writeBoolean(false);
+		dos.writeInt(33);
+		
+		dos.flush();
+		dos.close();
+		
+		DataInputStream dis = new DataInputStream(new FileInputStream("prim.dat"));
+		double a = dis.readDouble();
+		double b = dis.readDouble();
+		boolean c = dis.readBoolean();
+		int i = dis.readInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(i);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
